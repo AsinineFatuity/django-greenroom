12a3dc47145468a12317692c4fa22c52a2c952c8
kembo <asininefatuity@gmail.com>
2026-06-07 11:50:43 +0300

    feat: hooked error boundary

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,11 +1,18 @@
 import Home from "@/pages/Home";
 import { createBrowserRouter } from "react-router-dom";
+import RouteErrorBoundary from "./RouterErrorBoundary";
+import FallBackErrorUI from "./FallBackErrorUI";
 import { APP_URLS } from "@/helpers";
 
 const router = createBrowserRouter([
     {
-        path: APP_URLS.home,
-        element: <Home />,
+        errorElement: <RouteErrorBoundary fallback={<FallBackErrorUI />} />,
+        children: [
+            {
+                path: APP_URLS.home,
+                element: <Home />,
+            },
+        ],
     },
 ]);
 
